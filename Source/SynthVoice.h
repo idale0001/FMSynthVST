ba968e54e3fe2abc123ee04ef9b4da8970b0171d
Isaac Dale <idale0001@gmail.com>
2020-09-05 22:41:01 -0400

    Cleaned sound generation and set tuning to 440hz

--- Source/SynthVoice.h
+++ Source/SynthVoice.h
@@ -22,31 +22,43 @@ public:
         return dynamic_cast<SynthSound*>(sound) != nullptr;
     }
     void startNote(int midiNoteNumber, float velocity, SynthesiserSound* sound, int currPitchWheelPosition) {
-        level = velocity;
-        frequency = MidiMessage::getMidiNoteInHertz(midiNoteNumber);
+        currentAngle = 0.0;
+        level = velocity * 0.15;
+
+        frequency = juce::MidiMessage::getMidiNoteInHertz(midiNoteNumber);
+        double cyclesPerSample = frequency / getSampleRate();
+
+        angleDelta = cyclesPerSample * 2 * juce::MathConstants<double>::pi;
     }
 
     void stopNote(float velocity, bool allowTailOff) {
         level = velocity;
         frequency = 0;
+        currentAngle = 0;
+        angleDelta = 0;
         clearCurrentNote();
     }
 
     void pitchWheelMoved(int newPitch) {
-
+        frequency = MidiMessage::getMidiNoteInHertz(newPitch);
     }
 
     void controllerMoved(int collerNumber, int newControllerValue) {
 
     }
 
+    void setSampleRate(int sampleRate) {
+        currentSampleRate = sampleRate;
+    }
+
     void renderNextBlock(AudioBuffer<float>& outputBuffer, int startSample, int numSamples) {
-        angleDelta = frequency / 24000;
-        for (int sample = 0; sample < numSamples; sample++) {
-            double currAmp = std::sin(currentAngle);
-            currentAngle += angleDelta;
-            for (int channel = 0; channel < outputBuffer.getNumChannels(); channel++) {
-                outputBuffer.addSample(channel, startSample + sample, currAmp);
+        if (angleDelta != 0) {
+            for (int sample = 0; sample < numSamples; sample++) {
+                double currAmp = std::sin(currentAngle) * level;
+                currentAngle += angleDelta;
+                for (int channel = 0; channel < outputBuffer.getNumChannels(); channel++) {
+                    outputBuffer.addSample(0, startSample + sample, currAmp);
+                }
             }
         }
     }
